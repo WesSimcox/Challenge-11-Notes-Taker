e16b09e5546cdaea669cdea4feab1ec1b86e3fc0
Wes Simcox <wes.simcox11@gmail.com>
2023-08-02 12:08:12 -0600

    adding routes

---
+++ server.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { notes } = require("./db/db.json");
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+const generateUniqueId = require('generate-unique-id');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = generateUniqueId();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
